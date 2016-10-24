17718e188dbf8f517ee3da4cca39e7b69241efc2
Jake Larson <g2n8@bowen.ugrad.cs.ubc.ca>
2016-10-24 13:59:13 -0700

    lab6 finished

--- lab6/qsortCount.cc
+++ lab6/qsortCount.cc
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <cstdlib>
 
+using namespace std;
 int * x;
 int totalComps;
 
@@ -17,11 +18,8 @@ int randint(int a, int b) {
 
 int c (int  n){
     if(n > 1){
-        int sum = 0;
-        for(int m=1; m<=n; m++){
-            sum += n-1 + c(m-1) + c(n-m);
-        }
-        return sum;
+        int r = randint(0,n-1);
+        return  n-1 + c(r) + c(n-r);
     } else {
         return 0;
     }
@@ -41,8 +39,8 @@ void quicksort(int a, int b) {
 	for (i = a+1; i <= b; i++) {
 		if (x[i] < x[a])
 			swap(x[++m], x[i]);
-        totalComps++;
-    }
+        	totalComps++;
+    	}
 	swap(x[a],x[m]);
 	quicksort(a, m-1);
 	quicksort(m+1, b);
@@ -52,11 +50,10 @@ void quicksort(int a, int b) {
 #define NREPS 100
 
 int main(int argc, char *argv[]) {
-	srand(time(0));
+    srand(time(0));
     
     int averageComps = 0;
     int quicsortComparisons = 0;
-    int curComparisons = 0;
     
     for(int j=0; j<NREPS; ++j){
         totalComps = 0;
@@ -71,18 +68,31 @@ int main(int argc, char *argv[]) {
         quicksort(0, NN-1);
         
         for (int i=0; i<NN; ++i) {
-            std::cout << x[i] << " ";
+            cout << x[i] << " ";
         }
+	cout << endl;
         averageComps += totalComps;
         quicsortComparisons += c(NN);
-        std::cout << std::endl;
         
         delete[] x;
     }
-    averageComps = averageComps/REP;
-    quicsortComparisons = quicsortComparisons/REP;
     
-    std::cout << averageComps << " " << quicsortComparisons << '\n';
+    cout << endl << endl;
+    averageComps = averageComps/NREPS;
+    quicsortComparisons = quicsortComparisons/NREPS;
     
-	return 0;
+    cout << "Ave Comp: " << averageComps << "   " << "Estimated Avg Comp: " << quicsortComparisons << '\n';
+    
+    return 0;
 }
+
+/*
+worst case: n^2 (depends on pivot, but usually when the elements are already sorted it preforms worse)
+best case : nlgn
+avg case  : nlgn
+
+it is an inplace sort as there aren't any other data structures we use other than th einitial array and a few other variables to store things as we move them
+
+quicksort is not a stable sort, doesn't have the same output if the key is different
+
+*/
