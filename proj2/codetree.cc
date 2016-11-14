6b4e03c66e7d307c5b6f21352fb72672d5f6db2f
Justin Wong <e2y8@bowen.ugrad.cs.ubc.ca>
2016-11-14 15:46:13 -0800

    added some preliminary prototypes

--- proj2/codetree.cc
+++ proj2/codetree.cc
@@ -1,1 +1,42 @@
 #include <codetree.h>
+
+class CodeTree {
+    
+	int* treeHeap;
+
+  public:
+    void printTree ();
+    void printCode ();
+};
+
+CodeTree::CodeTree (int* heap){
+	treeHeap = heap;
+}
+
+//function prototype
+void printChar( int ch );
+
+
+void printTree(){
+
+
+}
+
+
+void printCode() {
+
+
+}
+
+
+
+void printChar( int ch ) {
+	if( ch < 16 ) {
+		std::cout << "x0" << std::hex << ch;
+	} else if( ch < 32 || ch > 126 ) {
+		std::cout << "x" << std::hex << ch;
+	} else {
+		std::cout << "\"" << (char)ch << "\"";
+	}
+}
+
