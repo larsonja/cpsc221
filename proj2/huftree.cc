21c37d5f691e8484057cb97a2376711af81a9e7c
Justin Wong <e2y8@bowen.ugrad.cs.ubc.ca>
2016-11-14 19:57:38 -0800

    inclusion issue

--- proj2/huftree.cc
+++ proj2/huftree.cc
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <algorithm>
 #include <vector>
-//#include "codetree.h"
+//#include "codetree.cc"
 #include <iomanip>
 #include <string>
 #include <deque>
@@ -61,6 +61,33 @@ void insert(vector<Node> heap, Node toInsert){
     heapUp(heap, heap.size() - 1);
 }
 
+void usage() {
+  std::cerr << "Usage: huftree text.txt" << std::endl;
+  throw std::exception();
+}
+
+
 int main (int argc, char* argv[]) {
-    return 0;
+  if( argc != 2 ) {
+    usage();
+  }
+
+  int freq[256] = { 0 };    // array initialized to 0
+  
+  std::ifstream fin( argv[1] );
+  if( fin.is_open() ) {
+    char ch;
+    while (fin >> std::noskipws >> ch) {    // don't skip whitespace
+      freq[(int)ch]++;
+    }
+
+    fin.close();
+  }
+  
+  CodeTree ct = CodeTree( freq );
+
+  ct.printTree();
+  ct.printCode();
+
+  return 0;
 }
