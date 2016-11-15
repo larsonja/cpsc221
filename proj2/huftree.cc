47605c5ca06ce6b6a6691fe45976c3ef94948d5e
larsonja <larsonjakea@gmail.com>
2016-11-14 18:53:10 -0800

    huftree compiles now

--- proj2/huftree.cc
+++ proj2/huftree.cc
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <algorithm>
 #include <vector>
-#include "codetree.h"
+//#include "codetree.h"
 #include <iomanip>
 #include <string>
 #include <deque>
@@ -14,16 +14,52 @@ class Node{
         int character; //ASCII code of the char
         int freq; //frequency at which is appears
     public:
-        int setChar(int charCode) {this->character = charCode;}
-        int setFreq(int frequency) {this->freq = frequency;}
-        int getChar(const Node& node) {return character;}
-        int getFreq(const Node& node) {return frequency;}
+        void setChar(int charCode) {this->character = charCode;}
+        void setFreq(int frequency) {this->freq = frequency;}
+        int getFreq(void) {return this->freq;}
+        int getChar(const Node& node) {return this->character;}
+        
 };
 
-void swapDown(vector<Node>) {
+//
+void heapify(vector<Node> heap, int i) {
+    int left = 2*i;
+    int right = 2*i + 1;
+    int largest = i;
     
+    int lF = heap[left].getFreq();
+    int rF = heap[right].getFreq();
+    int laF = heap[largest].getFreq();
+    int s = heap.size();
+    
+    if (left <= s - 1 && lF > laF) {
+        largest = left;
+    }
+    if (right <= s - 1 && rF > laF){
+        largest = right;
+    }
+    if (largest != i){
+        swap(heap[i], heap[largest]);
+        heapify(heap, largest);
+    }
+}
+
+void heapUp(vector<Node> heap, int index){
+    if (index == 0) return;
+    int iF = heap[index].getFreq();
+    int jF = heap[(index-1)/2].getFreq();
+    if (iF > jF){
+        //swap them and run again
+        swap(heap[index], heap[index-1]);
+        heapUp(heap, (index-1)/2);
+    }
+}
+
+void insert(vector<Node> heap, Node toInsert){
+    heap.push_back(toInsert);
+    heapUp(heap, heap.size() - 1);
 }
 
 int main (int argc, char* argv[]) {
-    
+    return 0;
 }
