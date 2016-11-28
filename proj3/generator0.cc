1fd6c01bca611b747b2e5b73e0e6de837d5f3523
larsonja <larsonjakea@gmail.com>
2016-11-28 01:32:14 -0800

    added starting proj3

---
+++ proj3/generator0.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib> // for atoi
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {// remember, argv[0] is the program name
+		cerr << "Wrong number of arguments!" << endl;
+		return 1;
+	}
+	int n = atoi(argv[1]);
+	for(int i = 1; i <= n/2; i++) {
+		cout << "I " << put something here... << endl;
+		cout << "R " << put something here... << endl;
+	}
+	return 0;
+}
