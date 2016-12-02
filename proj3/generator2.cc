7c7e6b951e734000250251e5e2dcdd2e4e9f65b2
Jake Larson <g2n8@bowen.ugrad.cs.ubc.ca>
2016-12-02 13:44:34 -0800

    added other generators

---
+++ proj3/generator2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib> // for atoi
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {// remember, argv[0] is the program name
+		cerr << "Wrong number of arguments!" << endl;
+		return 1;
+	}
+	int n = atoi(argv[1]);
+	for(int i = 1; i <= n/3; i++) {
+		cout << "I " << n << endl;
+	}
+	for(int i = 1; i <= n/3; i++) {
+		cout << "F " << n << endl;
+	}
+	for(int i = 1; i <= n/3; i++){
+		cout << "R " << n << endl;
+	}
+	return 0;
+}
